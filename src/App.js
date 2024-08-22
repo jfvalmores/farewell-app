267b56dcd6984f5fab9bef75ce2d4f5bb021a382
Justine Valmores <jfvalmores@gmail.com>
2024-08-22 23:04:55 +0800

    Justine's message

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
 import { useState } from 'react';
 
-import VijilProfile from './images/vijil.jpeg';
-import FredrikProfile from './images/fredrik.jpeg';
-import NorbertProfile from './images/norbert.jpeg';
-import PhilipProfile from './images/philip.png';
-import JustineProfile from './images/justine.jpeg';
-import EnriqueProfile from './images/enrique.jpeg';
-import JayProfile from './images/jay.jpeg';
 import './App.css';
+import { MainPerson, Persons } from './persons';
 
 function App() {
   const { width, height } = useWindowSize();
@@ -18,10 +12,14 @@ function App() {
   return (
     <div className='app'>
       <div className='header'>
-        <img className='main-character profile-image' src={EnriqueProfile} alt='enrique' />
+        <img
+          className='main-character profile-image'
+          src={MainPerson.image}
+          alt={MainPerson.name}
+        />
         {hasConfetti && <Confetti className='confetti' width={width} height={height} />}
-        <h1>Farewell Enrique!</h1>
-        <p>Thank you for being an awesome teammate.</p>
+        <h1>{MainPerson.header}</h1>
+        <p>{MainPerson.description}</p>
         <button
           className='confetti-toggle'
           type='button'
@@ -30,73 +28,13 @@ function App() {
           🎉
         </button>
       </div>
-      <div className='messages' title='Vijil'>
-        <div className='message'>
-          <img className='profile-image' src={VijilProfile} alt='vijil' />
-          <p className='post'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
-            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
-            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
-            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
-            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta
-            faucibus arcu eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu
-            libero fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse
-            potenti. Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus
-            vitae tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta
-            faucibus arcu eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu
-            libero fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse
-            potenti. Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus
-            vitae tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta
-            faucibus arcu eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu
-            libero fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse
-            potenti. Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus
-            vitae tristique.
-          </p>
-        </div>
-        <div className='message' title='Fredrik'>
-          <img className='profile-image' src={FredrikProfile} alt='fredrik' />
-          <p className='post'>
-            Thanks for all the great work and being a great colleague! Wish you the best! 🎉🎉🎉
-          </p>
-        </div>
-        <div className='message' title='Norbert'>
-          <img className='profile-image' src={NorbertProfile} alt='norbert' />
-          <p className='post'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
-            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
-            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
-            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
-            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </div>
-        <div className='message' title='Philip'>
-          <img className='profile-image' src={PhilipProfile} alt='philip' />
-          <p className='post'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
-            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
-            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
-            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
-            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </div>
-        <div className='message' title='Justine'>
-          <img className='profile-image' src={JustineProfile} alt='justine' />
-          <p className='post'>
-            Good luck bro. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta
-            faucibus arcu eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu
-            libero fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque.
-          </p>
-        </div>
-        <div className='message' title='Jay'>
-          <img className='profile-image' src={JayProfile} alt='jay' />
-          <p className='post'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
-            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
-            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
-            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
-            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </p>
-        </div>
+      <div className='messages'>
+        {Persons.map((person) => (
+          <div className='message' title={person.name}>
+            <img className='profile-image' src={person.image} alt={person.name} />
+            <p className='post'>{person.message}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/persons.js
@@ -0,0 +1,77 @@
+import VijilProfile from './images/vijil.jpeg';
+import FredrikProfile from './images/fredrik.jpeg';
+import NorbertProfile from './images/norbert.jpeg';
+import PhilipProfile from './images/philip.png';
+import JustineProfile from './images/justine.jpeg';
+import EnriqueProfile from './images/enrique.jpeg';
+import JayProfile from './images/jay.jpeg';
+
+export const MainPerson = {
+  description: 'Thank you for being an awesome teammate.',
+  header: 'Farewell Enrique!',
+  image: EnriqueProfile,
+  name: 'Enrique',
+};
+
+export const Persons = [
+  {
+    image: VijilProfile,
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
+            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
+            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
+            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
+            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta
+            faucibus arcu eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu
+            libero fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse
+            potenti. Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus
+            vitae tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta
+            faucibus arcu eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu
+            libero fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse
+            potenti. Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus
+            vitae tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta
+            faucibus arcu eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu
+            libero fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse
+            potenti. Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus
+            vitae tristique.`,
+    name: 'Vijil',
+  },
+  {
+    image: FredrikProfile,
+    message: `Thanks for all the great work and being a great colleague! Wish you the best! 🎉🎉🎉`,
+    name: 'Fredrik',
+  },
+  {
+    image: NorbertProfile,
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
+            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
+            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
+            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
+            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    name: 'Norbert',
+  },
+  {
+    image: PhilipProfile,
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
+            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
+            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
+            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
+            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    name: 'Philip',
+  },
+  {
+    image: JustineProfile,
+    message: `You’ve been an incredible teammate, and your contributions have greatly advanced our team’s progress. 
+            Your hard work and dedication have not gone unnoticed. 
+            Wishing you all the best in your future endeavors—I'm confident you’ll continue to achieve great things.`,
+    name: 'Justine',
+  },
+  {
+    image: JayProfile,
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta faucibus arcu
+            eget aliquet. Donec porta velit efficitur rhoncus tempus. Sed in ligula eu libero
+            fringilla hendrerit ac at nibh. Ut euismod vulputate pellentesque. Suspendisse potenti.
+            Donec feugiat enim nec sem ultrices blandit. Pellentesque rhoncus lacinia purus vitae
+            tristique. Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    name: 'Jay',
+  },
+];
