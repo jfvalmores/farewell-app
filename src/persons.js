145f7f4b7cbd91849c74bfbb74277744a6e153b0
Justine Valmores <justine.valmores@postnord.com>
2024-10-29 20:15:56 +0800

    update boilerplate

--- src/persons.js
+++ src/persons.js
@@ -1,17 +1,43 @@
-// import MainPersonProfile from './images/main-person-profile.jpeg';
-// import PersonProfile from './images/person-profile.jpeg';
+import VijilProfile from './images/vijil.jpeg';
+import FredrikProfile from './images/fredrik.jpeg';
+import NorbertProfile from './images/norbert.jpeg';
+import PhilipProfile from './images/philip.png';
+import JustineProfile from './images/justine.jpeg';
+import JayProfile from './images/jay.jpeg';
 
 export const MainPerson = {
-  // description: 'Thank you for being an awesome teammate.',
-  // header: 'Farewell Main Person!',
-  // image: MainPersonProfile,
-  // name: 'Main Person',
+  description: 'Thank you for being an awesome teammate.',
+  header: 'Farewell Vijil!',
+  image: VijilProfile,
+  name: 'Vijil',
 };
 
 export const Persons = [
-  // {
-  //   image: PersonProfile,
-  //   message: `We want to say something meaningful. But we forgot.`,
-  //   name: 'Person',
-  // },
+  {
+    image: FredrikProfile,
+    message: `Thanks for all the great work and being a great colleague! Wish you the best! 🎉🎉🎉`,
+    name: 'Fredrik',
+  },
+  {
+    image: NorbertProfile,
+    message: `I just wanted to say thanks for being such an awesome colleague. You brought some great ideas to the table and really helped with the audit storage revamp. It’s been great working with you, and I’m really going to miss our teamwork. Best of luck with everything ahead!`,
+    name: 'Norbert',
+  },
+  {
+    image: PhilipProfile,
+    message: `Crazy to think it's been almost 1 year and 6 months since you joined already. Time flies when you're having fun! Thanks for all the hard work and great contributions, I wish you all the best in your next adventure!`,
+    name: 'Philip',
+  },
+  {
+    image: JustineProfile,
+    message: `You’ve been an incredible teammate, and your contributions have greatly advanced our team’s progress. 
+            Your hard work and dedication have not gone unnoticed. 
+            Wishing you all the best in your future endeavors—I'm confident you’ll continue to achieve great things. 🚀`,
+    name: 'Justine',
+  },
+  {
+    image: JayProfile,
+    message: `This is Jay.`,
+    name: 'Jay',
+  },
 ];
